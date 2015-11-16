14c54ce8a77d684fbbcb0a7027acc8ea165598b0
Tim Clark <tim.clark@inforeliance.com>
2015-11-16 14:33:22 -0500

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myAwesomeAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myAwesomeAppApp
+ */
+angular.module('myAwesomeAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.stuff="Whatever";
+    $scope.morestuff="abc";
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.ShowSweetAlert = function(){
+      sweetAlert("Oops...", "Something went wrong!", "success");
+    };
+  });
