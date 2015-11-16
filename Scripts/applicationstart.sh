45b20562254278cd50bda2cad38b4d96a65823c4
unknown <ross.serino@IPRW-R907GUBA.inforeliance.com>
2015-11-16 14:49:22 -0500

    added appspec / scripts

---
+++ Scripts/applicationstart.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+. ~/.bashrc
+
+export NODE_ENV=production
+
+cd /var/www/donorschoose/
+npm update
+npm install
+bower install
+
+grunt build --force
+
+cd /var/www/donorschoose/dist/server
+
+echo "Starting NodeJS"
+forever start app.js >> /home/ec2-user/nodelog.log 2> /dev/null < /dev/null &
+echo "NodeJS started"
+
+cd /var/www/donorschoose/
+grunt build --force
+
+exit 0
+
+exit
